2791a6df6fdb0235e65f1419ae945acd61fe279a
Siarhei Sobal <serhei.sobal@gmail.com>
2024-09-10 17:28:43 +0300

    cors policy

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const taskRoutes = require("./routes/taskRoutes");
-require("./utils/cronJob"); // Запуск cron
+require("./utils/cronJob");
 
 const app = express();
-app.use(express.json());
 
+const allowedOrigins = process.env.CORS_ORIGIN
+  ? process.env.CORS_ORIGIN.split(",")
+  : [];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
 app.use(taskRoutes);
 
 const PORT = process.env.PORT || 2000;
